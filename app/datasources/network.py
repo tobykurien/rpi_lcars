816e9cca8ff3d51c5be527d80d3395c36f666380
Greg Lindsey <greg.lindsey@yello.co>
2018-06-04 08:29:49 -0500

    Updated to make code gracefully handle connection errors.

--- app/datasources/network.py
+++ app/datasources/network.py
@@ -1,8 +1,37 @@
+import errno
 import socket
 
 
 def local_ip_address():
-    "Returns the IP address of the machine we're running on."
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(('8.8.8.8', 1))
-    return s.getsockname()[0]
+    """
+    Returns the IP address of the machine we're running on.
+
+    If the test socket cannot connect for any reason, returns a string that
+    attempts to describe the problem (sort of). At the moment, a machine that
+    is connected to a LAN that doesn't have a route to the internet will return
+    'LOCAL ONLY'; a different method of obtaining the IP address will probably
+    be needed in those instances.
+
+    In the future this function should possibly be converted to a service that
+    automatically updates if the status of the network changes, but that would
+    obviously be a large overhaul of the entire system (basically switching
+    widgets to a publish/subscribe model).
+    """
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 1))
+        result = s.getsockname()[0]
+    except IOError as e:
+        result = errno_message(e.errno)
+
+    return result
+
+
+def errno_message(err):
+    lookup = {
+        100: 'OFFLINE',
+        101: 'NO NETWORK',
+        113: 'LOCAL ONLY'
+    }
+    return lookup.get(err, 'NET ERROR {0}'.format(err))
