14b836151a46773d9af760cc6444889d105eebbf
Toby Kurien <toby@tobykurien.com>
2018-06-14 17:37:48 +0200

    added python-psutil dependency

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install python-pygame python-pil
+sudo apt-get install python-pygame python-pil python-psutil
 
 # on dietpi/raspbian lite install xinit
 sudo apt-get install xinit
-
